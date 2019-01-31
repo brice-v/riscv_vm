d62941450453dfc58efe8cc9f0189c18b0e3a0dc
brice-v <30622883+brice-v@users.noreply.github.com>
2019-01-31 13:56:19 -0500

    Update riscvm.nim

--- riscvm.nim
+++ riscvm.nim
@@ -277,3 +277,169 @@ proc store(offset: uint16, src: uint8, base: uint8, width: uint8) =
     ## rs1 is base
     ## offset is 12 bits
 
+
+################################################################
+#
+# #### ##### ##### MEMORY MODEL INSTRUCTIONS  ##### ##### #### #
+#
+################################################################
+
+## see section 2.7 page 20+ 
+
+## `hart` -> Hardware thread
+##
+## Each `hart` has its own register state and program counter,
+## and executes an independent sequential instruction stream
+##
+## `hart`s can communicate with other harts via calls to the
+## execution environment, or directly via the shared mem system
+##
+## `hart`s can also interact with I/O devices and indirectly with
+## each other via loads and stores to portions of the address space
+## assigned to the I/O
+
+proc fence(predecessor: uint8, successor: uint8) =
+    ##
+    ## FENCE is used to order device I/O and memory accesses
+    ## as viewed by other harts and external devices and coprocessors
+    ## 
+    ## KEY: I -> Input, O -> Ouput, R -> Read, W -> Write
+    ##      P -> Predecessor,   S -> Successor
+    ##
+    ## Informally, no other hart can observe any operation in the
+    ## successor set following a FENCE before any operation in the
+    ## predecessor set preceeding the FENCE
+
+proc fencei(imm_val: uint16, rs1: uint8, rd: uint8) =
+    ##
+    ## FENCE.I is used to synchronize the instruction and data streams
+    ##
+    ## RISC-V does not guarantee that stores to instruction memory will
+    ## be made visible to the instruction fetches on the same hart
+    ## until a FENCE.I instruction is executed
+    ##
+    ## FENCE.I only ensures that a subsequent instruction fetch on a hart
+    ## will see any previous data stores already visible to the same hart
+    ##
+    ## To make a store to instruction memory visble to all harts the writing
+    ## hart has to execute a data FENCE before requesting that all remote
+    ## harts execute a FENCE.I
+    ##
+    ## NOTE: imm[11:0], rs1, rd are reserved for finer grained control
+    ## just zero them for now
+
+
+################################################################
+#
+###### ##### ##### ##### SYSTEM INSTRUCTIONS ##### ##### ##### #
+#
+################################################################
+
+## see section 2.8 page 21+
+
+## SYSTEM instructions are used to access system functionality
+## that might require priveledged access
+##
+## 2 Main Classes: Those that atomically read-modify-write control
+## and status registers (CSR) (and all other priveledged instructions)
+
+
+## ##### ##### CSR INSTRUCTIONS ##### ##### ##
+
+proc csrrw(imm_val:uint16, rs1: uint8, rd: uint8) =
+    ##
+    ## CSRRW (Atomic Read/Write CSR) atomically swaps
+    ## values in the CSRs and integer registers
+    ##
+    ## CSRRW reads the old value of the CSR, 0 extends the value to 
+    ## XLEN bits, then writes it to integer register rd
+    ##
+    ## The initial value in rs1 is written to CSR
+    ##
+    ## If rd=x0 then the instruction shall not read the CSR and shall
+    ## not cause any of the side effects that might occur on a CSR read
+
+proc csrrs(imm_val:uint16, rs1: uint8, rd: uint8) =
+    ##
+    ## CSRRS (Atomic Read and Set bits in CSR) reads the value of the
+    ## CSR, 0 extends it to XLEN bits and writes it to rd
+    ##
+    ## The initial value in rs1 is treated as a bit mask that specifies
+    ## bit positions to be set in the CSR
+
+proc csrrc(imm_val:uint16, rs1: uint8, rd: uint8) =
+    ##
+    ## CSRRC (Atomic Read and Clear bits in CSR) reads the value of the 
+    ## CSR, 0 extends it to XLEN bits and writes it to rd
+    ##
+    ## The initial value in rs1 is treated as a bit mask that specifies
+    ## the bit positions to be cleared in the CSR
+
+## FOR BOTH CSRRS and CSRRC: if rs1=x0 then the instruction will not write
+## to the CSR at all and shall not cause any side effects that might otherwise
+## on a CSR write
+
+## CSRRWI, CSRRSI, and CSRRCI: similar to above except that they
+## update the CSR using an XLEN bit value obtained by 0 extending
+## a 5 bit unsigned immediate field encoded in the rs1 field instead
+## of a value from an integer register
+##
+## FOR CSRRSI and CSRRCI if the uimm field is 0 then these instructions will
+## not write to the CSR and shall not cause any of the side effects that might
+## otherwise occurs
+
+proc csrrwi(imm_val:uint16, rs1: uint8, rd: uint8) =
+
+proc csrrsi(imm_val:uint16, rs1: uint8, rd: uint8) =
+
+proc csrrci(imm_val:uint16, rs1: uint8, rd: uint8) =
+
+
+## ##### ##### TIMER AND COUNTERS INSTRUCTIONS ##### ##### ##
+
+
+## RV32I provides a number of 64-bit read only user level counters which
+## are mapped into the 12-bit CSR address space and accessed in 32-bit
+## using the CSRRS instructions
+
+## RDCYCLE
+## RDCYCLEH
+## RDTIME
+## RDTIMEH
+## RDINSTRET
+## RDINSTRETH
+
+
+## IGNORING THESE FOR NOW ##
+
+
+## ##### ### ENVIRONMENT CALL AND BREAK POINTS ### ##### ##
+
+proc ecall() =
+    ##
+    ## ECALL is used to make a request to the supporting execution
+    ## environment (usually an os)
+    ## 
+    ## The ABI for the system will define how parameters for the
+    ## environment request and passed, but usually these will be in
+    ## defined locations in the integer register file
+
+
+proc ebreak() =
+    ##
+    ## EBREAK is used by debuggers to cause control to be transferred
+    ## back to a debugging environment
+
+
+###########################################################
+#                                                         #
+################# END OF RV32I INSTRUCTIONS ###############
+#                                                         #
+###########################################################
+
+
+
+
+# mul     rd rs1 rs2 31..25=1 14..12=0 6..2=0x0C 1..0=3
+
+# 1111111XXXXXXXXXX000XXXXX0110011
